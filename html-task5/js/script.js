cab29a386070a82c15ec5ef3ba37ff1099ef6555
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-10-15 08:31:34 +0300

    Update script.js

--- html-task5/js/script.js
+++ html-task5/js/script.js
@@ -1,46 +1,15 @@
 button.onclick = function() 
 {
-  let f1 = document.getElementsByName("cost");
-  let f2 = document.getElementsByName("count");
-  let r = document.getElementById("result");
+  let weight = document.getElementsByName("weight");
+  let cost = document.getElementsByName("item");
+  let result = document.getElementById("result");
   
-  if (!Number(f1[0].value) && !Number(f2[0].value)) 
+  if (!Number(weight[0].value) || Number(weight[0].value) < 0)
   {
-    alert("Поле 1 и 2 не являются числами");
-    r.innerHtML = none;
+    alert("Вес введен некорректно");
     return false;
   }
-  if (!Number(f1[0].value)) 
-  {
-    alert("Поле 1 не является числом");
-    r.innerHtML = none;
-    return false;
-  }
-  if (!Number(f2[0].value)) 
-  {
-    alert("Поле 2 не является числом");
-    r.innerHtML = none;
-    return false;
-  }
-  if (f1[0].value < 0 && f2[0].value < 0) 
-  {
-    alert("Поля 1 и 2 не могут быть отрицательными числами!!!");
-    r.innerHtML = none;
-    return false;
-  }
-  if (f1[0].value < 0) 
-  {
-    alert("Поле 1 не не может быть отрицательным!");
-    r.innerHtML = none;
-    return false;
-  }
-  if (f2[0].value < 0) 
-  {
-    alert("Поле 2 не может быть отрицательным!");
-    r.innerHtML = none;
-    return false;
-  }
-  
-  r.innerHTML = f1[0].value * f2[0].value;
+ 
+  result.innerHTML += weight[0].value * cost.value;
   return false;
 }
