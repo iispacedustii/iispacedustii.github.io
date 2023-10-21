bcd3218873251225707e5aef843252528b7c092d
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-10-21 16:33:00 +0300

    Update script.js

--- html-task5/js/script.js
+++ html-task5/js/script.js
@@ -1,26 +1,42 @@
-document.addEventListener("DOMContentLoaded", function (event) {  console.log("DOM fully loaded and parsed");  });
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM loaded");
+    let buttonA = document.getElementById("calculate");
+    buttonA.addEventListener("click", toCalculate);
+    let buttonB = document.getElementById("nulify");
+    buttonB.addEventListener("click", toNulify);
+});
 
-button.onclick = compute() 
+/*button.onclick = compute() 
 {
   let text = document.getElementById('text');
   let weight = document.getElementsByName("weight");
   let cost = document.getElementsByName("item");
   //let cost = cost.value;
-
-
-  
   if (!Number(weight.value) || Number(weight.value) <= 0)
-  {
-    alert("Вес введен некорректно");
-    return false;
-  }
-  
+  { alert("Вес введен некорректно"); return false; }
   text.innerHTML =  String(result + weight.value * cost.value) + "руб";
-  return false;
-}
+  return false;}
 
 button.onclick = nulify() 
 {
   result.innerHTML = 0;
   return false;
-}
+}*/
+
+let toCalculate = () => {
+    let weight = document.getElementById("weight").value;
+    let item = document.getElementById("item").value;
+  
+    if(!Number(weight.value) || Number(weight.value) <= 0)
+      return "Стоимость: " + String(Number[item] * Number(weight));
+    else
+      return "Некорректно введен вес";
+  
+    let domAns = document.getElementById("result");
+    domAns.replaceChildren(`${value}`);
+};
+
+let toNulify = () => {
+    let domAns = document.getElementById("result");
+    domAns.replaceChildren(`${0}`);
+};
