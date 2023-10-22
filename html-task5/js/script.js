25dcfa0fcef30510772373e00f19a3e54266e555
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-10-22 20:51:49 +0300

    Update script.js

--- html-task5/js/script.js
+++ html-task5/js/script.js
@@ -2,27 +2,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM loaded");
     let buttonA = document.getElementById("calculate");
     buttonA.addEventListener("click", toCalculate);
-    let buttonB = document.getElementById("nulify");
-    buttonB.addEventListener("click", toNulify);
 });
-
-/*button.onclick = compute() 
-{
-  let text = document.getElementById('text');
-  let weight = document.getElementsByName("weight");
-  let cost = document.getElementsByName("item");
-  //let cost = cost.value;
-  if (!Number(weight.value) || Number(weight.value) <= 0)
-  { alert("Вес введен некорректно"); return false; }
-  
-  text.innerHTML =  String(result + weight.value * cost.value) + "руб";
-  return false;}
-
-button.onclick = nulify() 
-{
-  result.innerHTML = 0;
-  return false;
-}*/
 
 let toCalculate = () => {
     let weight = document.getElementById("weight").value;
@@ -35,9 +15,4 @@ let toCalculate = () => {
   
     let domAns = document.getElementById("result");
     domAns.replaceChildren(`${value}`);
-};
-
-let toNulify = () => {
-    let domAns = document.getElementById("result");
-    domAns.replaceChildren(`${0}`);
 };
