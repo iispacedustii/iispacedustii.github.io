0840c037a0d49361ddf93b9f8b7fb7b2afeb2625
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-10-22 20:54:08 +0300

    Update script.js

--- html-task5/js/script.js
+++ html-task5/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     buttonA.addEventListener("click", toCalculate);
 });
 
-let toCalculate = () => {
+/*let toCalculate = () => {
     let weight = document.getElementById("weight").value;
     let item = document.getElementById("item").value;
   
@@ -15,4 +15,15 @@ let toCalculate = () => {
   
     let domAns = document.getElementById("result");
     domAns.replaceChildren(`${value}`);
+};*/
+
+let toCalculate = () => {
+    let weight = document.getElementById("weight").value;
+    let item = document.getElementById("item").value;
+
+    let domAns = document.getElementById("result");
+    if(!Number(weight.value) || Number(weight.value) <= 0)
+      domAns.innerHTML = "Стоимость: " + String(Number[item] * Number(weight));
+    else
+      domAns.innerHTML = "Некорректно введен вес";
 };
