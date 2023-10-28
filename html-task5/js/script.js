ba439d0b79b9645634b038ae4c5815ce38c4ceb7
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-10-28 15:55:40 +0300

    Update script.js

--- html-task5/js/script.js
+++ html-task5/js/script.js
@@ -1,11 +1,10 @@
-
 function toCalculate() {
     let weight = document.getElementById("weight");
     let item = document.getElementById("item");
     let domAns = document.getElementById("result");
     
     if(Number(weight.value) && weight.value > 0)
-      result.innerHTML = "Стоимость: " + String(Number(item[0].value) * Number(weight[0].value));
+      result.innerHTML = "Стоимость: " + String(Number(item.value) * Number(weight.value));
     else
       result.innerHTML = "Некорректно введен вес";
     
