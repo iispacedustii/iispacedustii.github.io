00ade67d24d7154404b645d10318f558428a43f2
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-11-06 23:18:07 +0300

    Update script_task6.js

--- html-task6/js/script_task6.js
+++ html-task6/js/script_task6.js
@@ -1,71 +1,19 @@
-/*<section class="service_calculator">
-                <h1>Калькулятор услуги</h1>
-                <div class="prices_info">
-                    <ul>
-                        <li>Бургер - 300 рублей</li>
-                        <li>Шаурма - 200 рублей (сырный лаваш +50 рублей)</li>
-                        <li>Картофель фри - 100 рублей (посыпать солью +20 рублей)</li>
-                    </ul>
-                </div>
-                <div class="calculator">
-                    <label for="amount_service">Количество:</label>
-                    <input id="amount_service" type="text">
-                    <ul class="service_choice">
-                        <li><input name="r" class="radio-choice" type="radio" value="Бургер">Бургер</li>
-                        <li><input name="r" class="radio-choice" type="radio" value="Шаурма">Шаурма</li>
-                        <li><input name="r" class="radio-choice" type="radio" value="Картофель фри">Картофель фри</li>
-                    </ul>
-                    <div id="add">
-                        <label for="additional">Лаваш:</label>
-                        <select name="additional" id="additional">
-                            <option value="Обычный лаваш" selected>Обычный</option>
-                            <option value="Сырный лаваш">Сырный</option>
-                        </select>
-                    </div>
-                    <div id="check">
-                        <label id="check_label" value="Посыпать солью">Посыпать солью.</label>
-                        <input type="checkbox" id="checkbox">
-                    </div>
-                    <div id="result_service">
-                        0
-                    </div>
-                </div>
-            </section>*/
-
-const additional_prices = {
-    "Сырный лаваш": 50,
-    "Обычный лаваш": 0
-};
-
-const salt_price = {
-    "true": 20,
-    "false": 0
-};
-
 function updatePrice() {
-    let VALUE = 0;
-    let radios = document.getElementsByName("r");
-    let radio_val = "Бургер";
+    let result_price = 0;
+    let main_price = document.getElementsByName("radio-6");
 
-    radios.forEach((radio) => {
-        if(radio.checked) {
-            radio_val = radio.value;
-            let radio_price = food_prices[radio.value];
-
-            if (radio_price !== undefined) {
-                VALUE += radio_price;
-            }
-        }
+    radios.forEach((main_price) => {
+        if(radio.checked)
+            result_price +=  Number(main_price.value);
     });
 
-    let sel = document.getElementById("add");
-    sel.style.display = (radio_val == "Шаурма") ? "block" : "none";
-    let selection = document.getElementById("additional");
+    let add_price = document.getElementById("type-select");
+    add_price.style.display = (main_price.value == 189) ? "block" : "none";
 
-    VALUE += additional_prices[selection.value];
+    result_price += Number(add_price.value);
 
     let check = document.getElementById("check");
-    check.style.display = (radio_val == "Картофель фри" ? "block" : "none");
+    check.style.display = (main_price == "Картофель фри" ? "block" : "none");
 
     document.getElementById("checkbox").checked ? VALUE += 20 : null; 
 
@@ -73,15 +21,15 @@ function updatePrice() {
     (input.value !== undefined) ?
         /^[0-9]+$/.test(input.value) ? VALUE *= Number(input.value) : null : null;
 
-    document.getElementById("result_service").innerHTML = `${VALUE} Рублей`;
+    document.getElementById("result_service").innerHTML = ${VALUE} + "руб.";
 }
 
 function reset() {
-    document.getElementById("additional").value = "Обычный лаваш";
+    document.getElementById("type-select").value = 0;
     document.getElementById("checkbox").checked = false;
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
+/*document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM loaded");
     reset();
     let radios = document.getElementsByName("r");
@@ -115,4 +63,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("checkbox").addEventListener("change", (event) => {
         updatePrice();
     });
-});
+});*/
