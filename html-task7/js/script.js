9e802ad0ecf85300a6627da734b3bff93c8f721d
Ivan Vlasenko <144020773+iispacedustii@users.noreply.github.com>
2023-11-12 17:28:19 +0300

    Update script.js

--- html-task7/js/script.js
+++ html-task7/js/script.js
@@ -1,1 +1,30 @@
 
+$(document).ready(() => {
+    $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+                }
+            },
+
+        ]
+    });
+});
